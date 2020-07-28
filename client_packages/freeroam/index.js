1217fe45fd96d952edc6f60b8dbe266e7ca0b4f3
TheNateFunction <60723327+thenatefunction@users.noreply.github.com>
2020-07-28 06:54:29 +0100

    Add location where shots are fired
    Add a method that tracks the event of a player shooting a gun and output the street name / crossing road into the chat box.

--- client_packages/freeroam/index.js
+++ client_packages/freeroam/index.js
@@ -10,6 +10,13 @@ let skinsinit    = require('./freeroam/menu_initialization/skins.js');
 let weaponInit   = require('./freeroam/menu_initialization/weapon.js');
 let playersInit  = require('./freeroam/menu_initialization/players.js');
 
+// Get the local player object
+const player = mp.players.local;
+// Initialize variables to pull street name and crossing road
+let getStreet = undefined;
+let streetName = undefined;
+let crossingRoad = undefined;
+
 // Creating browser.
 mp.events.add('guiReady', () => {
     if (!menu) {
@@ -30,4 +37,17 @@ mp.events.add('guiReady', () => {
             }
         });
     }
+});
+
+// Checks for the event of shots fired
+mp.events.add('playerWeaponShot', (targetPosition, targetEntity) => {
+	// If shots are fired by a player get the street name and crossing road from that players position
+    getStreet = mp.game.pathfind.getStreetNameAtCoord(player.position.x, player.position.y, player.position.z, 0, 0);
+    streetName = mp.game.ui.getStreetNameFromHashKey(getStreet.streetName); // Return string, if exist
+    crossingRoad  = mp.game.ui.getStreetNameFromHashKey(getStreet.crossingRoad); // Return string, if exist
+    if (streetName != '' && crossingRoad == ''){
+        mp.gui.chat.push('Shots Fired at: ' + streetName);
+    } else if (streetName != '' && crossingRoad != ''){
+        mp.gui.chat.push('Shots Fired at: ' + streetName + ' / ' + crossingRoad);
+	}
 });
