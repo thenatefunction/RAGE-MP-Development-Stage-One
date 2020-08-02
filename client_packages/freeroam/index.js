b6ecfc1dba6dbe13814eb78f5b959331b82440b9
TheNateFunction <60723327+thenatefunction@users.noreply.github.com>
2020-08-03 00:08:44 +0100

    Add better timer client-side handling
    Fixed how the timer was being handled. Timer now works properly for the client that shot the weapon. Also added a function to reset the location information if the timer reaches 10 minutes.

--- client_packages/freeroam/index.js
+++ client_packages/freeroam/index.js
@@ -1,14 +1,14 @@
 // CEF browser.
 let menu;
 // Configs.
-let vehicles     = JSON.parse(require('./freeroam/configs/vehicles.js'));
-let skins        = JSON.parse(require('./freeroam/configs/skins.js')).Skins;
-let weapon       = JSON.parse(require('./freeroam/configs/weapon.js'));
+let vehicles = JSON.parse(require('./freeroam/configs/vehicles.js'));
+let skins = JSON.parse(require('./freeroam/configs/skins.js')).Skins;
+let weapon = JSON.parse(require('./freeroam/configs/weapon.js'));
 // Initialization functions.
 let vehiclesInit = require('./freeroam/menu_initialization/vehicles.js');
-let skinsinit    = require('./freeroam/menu_initialization/skins.js');
-let weaponInit   = require('./freeroam/menu_initialization/weapon.js');
-let playersInit  = require('./freeroam/menu_initialization/players.js');
+let skinsinit = require('./freeroam/menu_initialization/skins.js');
+let weaponInit = require('./freeroam/menu_initialization/weapon.js');
+let playersInit = require('./freeroam/menu_initialization/players.js');
 
 // Get the local player entity
 const player = mp.players.local;
@@ -19,6 +19,11 @@ let getStreet = undefined;
 let streetName = undefined;
 let crossingRoad = undefined;
 let pingMessage;
+// Initialize wait timer
+let waitTimer;
+let minutes;
+let timerBool = false;
+let timeVar;
 
 // Creating browser.
 mp.events.add('guiReady', () => {
@@ -41,34 +46,67 @@ mp.events.add('guiReady', () => {
         });
     }
 });
+function setTimerBool(){
+	timerBool = false;
+	mp.events.callRemote("clearPingSent", (player));
+}
+function startTheTimer(){
+	timeVar = minutes;
+	if (timerBool == false){
+		waitTimer = setTimeout(setTimerBool(), timeVar);
+		timerBool = true;
+	} else if (timerBool == true){
+		clearTimeout(waitTimer);
+		waitTimer = setTimeout(setTimerBool(), timeVar);
+		timerBool = true;
+	}
+}
 
 // Call function for sending the location and status of area of shots fired
 let sendZoneToServer = () => {
-	// Pull the zone information for the player
+    // Pull the zone information for the player
     zone = mp.game.gxt.get(mp.game.zone.getNameOfZone(player.position.x, player.position.y, player.position.z));
-	if (zone != null){
-		mp.events.callRemote("zoneFiredIn", (player, zone));
-	} else {
-		mp.gui.chat.push("Zone doesn't exist");
-	}
+    if (zone != null) {
+        mp.events.callRemote("zoneFiredIn", (player, zone));
+    } else {
+        mp.gui.chat.push("Zone doesn't exist");
+    }
 };
 
 let sendPingMessage = () => {
-	if (pingMessage != null){
-		mp.events.callRemote("pingMessageReceived", (player, pingMessage));
-	}
+    if (pingMessage != null) {
+        mp.events.callRemote("pingMessageReceived", (player, pingMessage));
+    }
 };
 
 // Check for the event of a shot fired by a player
 mp.events.add('playerWeaponShot', (targetPosition, targetEntity) => {
-	sendZoneToServer();
+    sendZoneToServer();
 });
 
 let returnStreetInfo = (richOrPoorStr) => {
-	getStreet = mp.game.pathfind.getStreetNameAtCoord(player.position.x, player.position.y, player.position.z, 0, 0);
-	streetName = mp.game.ui.getStreetNameFromHashKey(getStreet.streetName);
+    getStreet = mp.game.pathfind.getStreetNameAtCoord(player.position.x, player.position.y, player.position.z, 0, 0);
+    streetName = mp.game.ui.getStreetNameFromHashKey(getStreet.streetName);
     crossingRoad = mp.game.ui.getStreetNameFromHashKey(getStreet.crossingRoad);
-	pingMessage = (richOrPoorStr + streetName + ' - ' + crossingRoad);
-	sendPingMessage();
+    pingMessage = (richOrPoorStr + streetName + ' - ' + crossingRoad);
+    sendPingMessage();
 };
 mp.events.add('returnAreaZone', returnStreetInfo);
+
+let startTimerFunc = (minutesForTheTimer) => {
+	if (waitTimer == null && timerBool == false){
+		minutes = minutesForTheTimer;
+		mp.gui.chat.push("Timer is activated.");
+		startTheTimer();
+	}
+};
+mp.events.add('startTimer', startTimerFunc);
+
+let startTimerFuncTwo = (minutesForTheTimerTwo) => {
+	if (waitTimer != null && timerBool == true){
+		minutes = minutesForTheTimerTwo;
+		mp.gui.chat.push("Timer is reset.");
+		startTheTimer();
+	}
+};
+mp.events.add('startTimerTwo', startTimerFuncTwo);
